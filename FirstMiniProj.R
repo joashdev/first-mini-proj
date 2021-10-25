6f5c7de7f3419192c6f14512a4daa79376640aac
Kent Joash Zamudio <kazamudio@up.edu.ph>
2021-10-25 15:26:33 +0800

    Added hospdata/* for problem 4. Modified FirstMiniProj.R for problem 4 solutions, and modified README.md to add function descriptions used in the FirstMiniProj.R

--- FirstMiniProj.R
+++ FirstMiniProj.R
@@ -1,11 +1,13 @@
-## NOTE: Function documentations are in the README.md of this repository
+## First Mini Project in CMSC 197
 
 ## Problem 1 - mean of a specified pollutant from a range of monitors
 pollutantmean <- function(directory, pollutant, id=1:332) {
   # ensure that directory ends with `/`
   directory <- if (endsWith(directory, "/")) directory else paste(directory, "/", sep = "")
-  csvs <- list.files(path = directory)[id]  # get csv files from directory
-  compiled_data <- data.frame() # variable for compiled data from csv files
+  # get csv files from directory
+  csvs <- list.files(path = directory)[id]  
+  # variable for compiled data from csv files
+  compiled_data <- data.frame() 
   
   # loop in each csv in csvs to get data
   for (csv in csvs){
@@ -21,11 +23,12 @@ pollutantmean <- function(directory, pollutant, id=1:332) {
 
 ## Problem 2 - number of complete observed cases from specified monitor/s
 complete <- function(directory, id = 1:332) {
-  
   # ensure that directory ends with `/`
   directory <- if (endsWith(directory, "/")) directory else paste(directory, "/", sep = "")
-  csvs <- list.files(path = directory)[id]  # get csv files from directory
-  complete_cases <- data.frame() # variable for complete cases
+  # get csv files from directory
+  csvs <- list.files(path = directory)[id]  
+  # variable for complete cases
+  complete_cases <- data.frame()
   
   # for loop in the range of id, using index is easier to get values from both
   # csvs and id since both have the same length
@@ -44,11 +47,15 @@ complete <- function(directory, id = 1:332) {
   complete_cases
 }
 
+## Problem 3 - get correlation between pollutants from monitors that has complete 
+##            cases greater than the specified threshold
 corr <- function(directory, threshold = 0) {
   # ensure that directory ends with `/`
   directory <- if (endsWith(directory, "/")) directory else paste(directory, "/", sep = "")
-  csvs <- list.files(path = directory)[1:332]  # get csv files from directory
-  correlations <- vector(mode="numeric") # numeric vector that will hold all correlations
+  # get csv files from directory
+  csvs <- list.files(path = directory)[1:332]  
+  # numeric vector that will hold all correlations
+  correlations <- vector(mode="numeric") 
   
   # loop in each csv from csvs to get data
   for(csv in csvs) {
@@ -66,3 +73,14 @@ corr <- function(directory, threshold = 0) {
   # return correlations
   correlations
 }
+
+## Problem 4 - modify the given code to plot the 30-day mortality rate from heart attack
+# read the csv file
+outcome <- read.csv('hospdata/outcome-of-care-measures.csv', colClasses = "character")
+# coerce the column 11 of `outcome` to be numeric instead of character
+outcome[, 11] <- as.numeric(outcome[, 11])
+# create a histogram from `outcome[, 11]`, 
+# use `xlab` to change the label in x-axis,
+# use the `main` argument to change the histogram title
+# and use the `col` argument to change the colors of the bars
+hist(outcome[, 11], xlab = "Deaths", main = "Hospital 30-Day Death(Mortality) Rates from Heart Attack", col = "lightblue")
--- README.md
+++ README.md
@@ -3,19 +3,42 @@
 ---
 #### Contents
 - [Description](#description)
-- [Solutions](#solutions)
+- [Functions](#functions)
 - [Contributors](#contributors)
 ---
 ### Description
 This repository is a mini-project in `CMSC 197 - Introduction to Data Science` which contains exercises in R Language
 
 ---
-### Solutions
-##### Pollutant Mean
-- **Problem:** Create a function `pollutantmean` that takes `directory`, `pollutant`, and `id` parameters and returns the `mean` of the specified pollutant. The `directory` is where the .csv files are located and the `id` are the monitor ID from which the data will be taken.
-- ** 
+### Functions
+1. `pollutantmean()` - this function returns the mean value of the specified pollutant from the given directory and monitor IDs
+    + params:
+        + `directory` | character vector | location of .csv files
+        + `pollutant` | character vector (`nitrate` or `sulfate`) | name of the pollutant
+        + `id` | integer vector (`default: 1:332`) | monitor IDs from which data will be retrieved
+    + returns:
+        + `numeric vector` of length 1
+    + execution:
+        + `pollutantmean("specdata", "nitrate", 23)`
+        + `pollutantmean("specdata", "sulfate", 70:72)`
+2. `complete()` - this function returns the number of complete observed cases from the given monitor IDs
+    + params:
+        + `directory` | character vector | location of .csv files
+        + `id` | integer vector (`default: 1:332`) | monitor IDs from which data will be retrieved from
+    + returns:
+        + `data.frame`
+    + execution:
+        + `complete("specdata", c(2,4,7,10))`
+        + `complete("specdata", 1)`
+3. `corr()` - this function returns the correlation between nitrate and sulfate in each monitor IDs whose complete cases are greater than the threshold
+    + params:
+        + `directory` | character vector | location of .csv files
+        + `threshold` | integer (`default: 0`) | lower limit of complete cases
+    + returns:
+        + `numeric vector`
+    + execution:
+        + `cr <- corr("specdata", 150) ; head(cr) ; summary(cr)`
 
-**Problem 2**<br>
 
 ---
 ### Contributors
